96e456fc2369bf3983cfcc6ee6afd8ff979749d8
Shevchik <Shev4ik.den@gmail.com>
2021-07-07 02:50:22 +0300

    Fix wrong import

--- src/protocolsupportresourcesgenerator/generators/locale/LocaleDataGenerator.java
+++ src/protocolsupportresourcesgenerator/generators/locale/LocaleDataGenerator.java
@@ -1,13 +1,12 @@
 package protocolsupportresourcesgenerator.generators.locale;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Writer;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Objects;
-
-import org.jline.utils.InputStreamReader;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
